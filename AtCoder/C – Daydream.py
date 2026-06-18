b49ec0acf6388f81286bdaf00a3974fa1406c1c8
Akshu.006 <163033737+AkshayaasreeKarthikeyaraj@users.noreply.github.com>
2026-06-18 10:24:18 +0530

    Add Daydream solution to AtCoder problem C

---
+++ AtCoder/C – Daydream.py
@@ -0,0 +1,16 @@
+S = input()
+if "eraser" in S:
+  S = S.replace("eraser","")
+if "dreamer" in S:
+  S = S.replace("dreamer","")
+if "dream" in S:
+  S = S.replace("dream","")
+if "erase" in S:
+  S = S.replace("erase","")
+
+if S != "":
+  print("NO")
+  
+else:
+  print("YES")
+  
